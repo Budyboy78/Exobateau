1aa38750ff74f1e0d66ed473c4d9fa7ba7cf19b6
Budyboy78 <remy.merle93@gmail.com>
2018-10-04 16:26:55 +0200

    salut

--- bateau.php
+++ bateau.php
@@ -1,13 +1,20 @@
-<?php 
-
-$tab = array (100);
-
-for($i=0, $i<0, $i++)
-    (
-        $tab[i] = rand(0,100);
-    )
-
-    for($i=0, $i<0, $i++)
-    (
-       echo  $tab[i] ;
-    )
+<?php
+$tab =array();
+for($i =0; $i<100;$i++)
+{
+    $tab[$i]=rand(0,100);
+}
+for($i =0; $i<101;$i++)
+{
+    
+$s=0;
+  for($j=0;$j<100;$j++)
+  {
+      if($tab[$j]==$i)
+       {
+           $s=$s+1;
+       }
+  }
+  echo "la valeur de ".$i." est presente ".$s." fois <br>";
+}
+?>
